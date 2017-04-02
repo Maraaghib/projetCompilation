dade9ff15a937e06f4d7dc12348d5a722174f1d1
Ahmad Boissetri Binzagr <abbinzagr@emi.u-bordeaux1.fr>
2017-04-02 12:49:04 +0200

    Maj interp.c si coherent avec ce que tu veux faire travaille direct dessus Clement plus sur ta branche paralelle

--- src/interp.c
+++ src/interp.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "interp.h"
+ #include <stdio.h> 
+ #include <stdlib.h>
+ #include <string.h>
+ #include "include/util.h"
+ #include "ppascalbison.h"
+
+ char *strdup(const char *s);
 
 TAS TAS_alloc(){
   return (TAS) malloc(sizeof(struct TAS));
@@ -48,3 +52,83 @@ int init_pile(TAS *tas, ADR *adr, TAL *tal){
   res &= init_tal(TAL);
   return res;
 }
+
+
+
+ int traitement(ENV *e,Noeud *n){
+	ENV pos; 
+ 	if (!n) return 0;
+ 	switch(n->tokentype){
+ 		case I:
+			return atoi(n->data);
+		case V:
+			{ pos=rech(n->data,e->debut);
+			return (pos->VAL);
+			}
+		case If:
+			traitement(e,n->gauche);
+                        traitement(e,n->droit);
+ 			return 0;
+		case El:
+			if(traitement(e,n->gauche)){
+ 				traitement(e,n->droit);
+				return 0;
+			}
+		case Th:
+			if(!traitement(e,n->gauche))
+ 				traitement(e,n->droit);
+			return 0;
+		case Wh:
+			if(traitement(e,n->gauche)){
+				traitement(e,n->droit);
+				traitement(e,n);
+				}
+		case Af:
+			initenv(e,n->gauche->data);
+ 			return affect(*e, n->gauche->data, traitement(e,n->droit));
+ 		case Se:
+ 			traitement(e,n->gauche);
+ 			return traitement(e,n->droit);
+ 		case Pl:
+ 		case Mo:
+ 		case Mu:
+			return eval(n->type, traitement(e, n->gauche), traitement(e, n->droit));
+		case And:
+			return(traitement(e, n->gauche) & traitement(e, n->droit));
+		case Or:
+			return(traitement(e, n->gauche) | traitement(e, n->droit));
+		case not:
+			return(!traitement(e,n->gauche));
+		case lt:
+			if(traitement(e, n->gauche) < traitement(e, n->droit))
+				return 1;
+			return 0;
+		case eq:
+			if(traitement(e, n->gauche) == traitement(e, n->droit))
+				return 1;
+			return 0;
+		case NewAr:	
+			return 0;
+		case T_int:
+			return atoi(n->data);
+		case T_bool:
+				return atoi(n->data);
+		case T_array:
+					return 0;
+			
+		case Def:
+			creer_bilfon(creer_fon(n->data,
+			;
+		
+     }
+     return 0;
+ }
+ 
+ int start(ENV *e,noeud* n){
+     int res = traitement(e,n);
+     ecrire_env(*e);
+     libererArbre(n);
+     return res;
+ }
+
+ 
