6d3cab90e58d2071bc7caa4039c048c0fc0ec9b7
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-04-04 01:33:46 +0200

    Ajout de définitions utiles pour l'interpréteur PP->C3A

--- include/bilquad.h
+++ include/bilquad.h
@@ -22,7 +22,12 @@ extern BILQUAD creer_bilquad(QUAD qd); /* retourne une biliste  a un element    
 extern QUAD rechbq(char *chaine, BILQUAD bq);/*ret le quad etiquete par chaine   */
 extern BILQUAD concatq(BILQUAD bq1, BILQUAD bq2);/* retourne la concatenation    */
 extern char *nomop(int codop); /* traduit entier vers chaine (= nom operation)    */
+extern char *gensym(char *prefix); /* fabrique de nouvelles chaines              */
+extern BILQUAD normal(BILQUAD bq);/* retourne la f.n. de bq: skip a la fin       */
+extern void ecrire_sep_bilquad(BILQUAD bq);/* affiche bq , avec separateurs ":"  */
 extern void ecrire_quad(QUAD qd); /* affiche le quadruplet                       */
 extern void ecrire_bilquad(BILQUAD bq); /* affiche la biliste de quadruplets     */
+
+
 
 #endif
