27addaa80b8f75f31cf66e65ef0de8d69bc95a8d
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-04-04 17:37:58 +0200

    Nettoyage du code

--- src/tradpp2c3a.c
+++ src/tradpp2c3a.c
@@ -17,122 +17,135 @@
 /*---------------------quadruplets-----------------------------------*/
 
 /* retourne une nouvelle chaine */
-char *gensym(char *prefix)
-{static int counter=0;
-  char *chaine;char *chcounter;
-  chcounter=Idalloc();
-  chaine=Idalloc();
-  strcpy(chaine,prefix);
-  sprintf(chcounter,"%d",counter);   /* prefix+chaine(counter)*/
-  counter=counter+1;
-  strcat(chaine,chcounter);
-  return(chaine);
+char *gensym(char *prefix) {
+    static int counter=0;
+    char *chaine;char *chcounter;
+    chcounter=Idalloc();
+    chaine=Idalloc();
+    strcpy(chaine,prefix);
+    sprintf(chcounter,"%d",counter);   /* prefix+chaine(counter)*/
+    counter=counter+1;
+    strcat(chaine,chcounter);
+    return(chaine);
 }
 
 /* test de gensym */
-void test_constantes(int n)
-{char *ident;int i=0;
-  for(i=0;i<n;i++)
-    {ident=gensym("CONS");
-      printf("%s \n",ident);
+void test_constantes(int n) {
+    char *ident;
+    for(int i = 0; i < n; i++) {
+        ident = gensym("CONS");
+        printf("%s \n",ident);
     }
 }
 
 
 /* retourne bq +  skip */
-BILQUAD ajouterskip(BILQUAD bq)
-{QUAD nq; BILQUAD nbq;
-  nq=creer_quad(gensym("ET"),Sk,NULL,NULL,NULL);/* instruction skip */
-  nbq=creer_bilquad(nq);
-  return(concatq(bq,nbq));
+BILQUAD ajouterskip(BILQUAD bq) {
+    QUAD nq;
+    BILQUAD nbq;
+    nq = creer_quad(gensym("ET"), Sk, NULL, NULL, NULL); /* instruction skip */
+    nbq = creer_bilquad(nq);
+    return (concatq(bq, nbq));
 }
 
 /* retourne la "forme normale" de bq: dernier quad = skip */
-BILQUAD normal(BILQUAD bq)
-{if (bq.fin== NULL)
-    {return(ajouterskip(bq));}
-  else
-    {if (bq.fin->OP!=Sk)        /* pas normal-> on normalise */
-	return(ajouterskip(bq));
-      else                      /* deja normal */
-	return(bq);}
+BILQUAD normal(BILQUAD bq) {
+    if (bq.fin == NULL) {
+        return(ajouterskip(bq));
+    }
+    else {
+        if (bq.fin->OP != Sk)        /* pas normal-> on normalise */
+            return(ajouterskip(bq));
+        else                      /* deja normal */
+            return(bq);
+    }
 }
 
 /* traduit entier (= codop) vers chaine (= nom operation)  */
-char *nomop(int codop)
-{switch(codop)
-    {case(I):return("I");
-    case(V):return("V");
-    case(Mp): return("Mp");
-    case(Af): return("Af");
-    case(Sk): return("Sk");
-    case(Se): return("Se");
-    case(If): return("If");
-    case(Th): return("Th");
-    case(El): return("El");
-    case(Wh): return("Wh");
-    case(Do): return("Do");
-    case(Pl): return("Pl");
-    case(Mo): return("Mo");
-    case(Mu): return("Mu");
-    case(Afc): return("Afc");
-    case(St): return("St");
-    case(Jp): return("Jp");
-    case(Jz): return("Jz");
-    case(halt):return("halt");
-    case(nop):return("nop");
-    case(rrmovl): return("rrmovl");
-    case(irmovl): return("irmovl");
-    case(rmmovl): return("rmmovl");
-    case(mrmovl): return("mrmovl");
-    case(addl): return("addl");
-    case(subl): return("subl");
-    case(andl): return("andl");
-    case(xorl): return("xorl");
-    case(jmp): return("jmp");
-    case(je): return("je");
-    case(jg): return("jg");
-    case(call): return("call");
-    case(ret): return("ret");
-    case(pushl): return("pushl");
-    case(popl): return("popl");
-    case(0):return("");                   /* code 0: directive assembleur y86 */
-    default:return(NULL);
+char *nomop(int codop) {
+    switch(codop) {
+        case(I): return("I");
+        case(V): return("V");
+        case(Mp): return("Mp");
+        case(Af): return("Af");
+        case(Sk): return("Sk");
+        case(Se): return("Se");
+        case(If): return("If");
+        case(Th): return("Th");
+        case(El): return("El");
+        case(Wh): return("Wh");
+        case(Do): return("Do");
+        case(Pl): return("Pl");
+        case(Mo): return("Mo");
+        case(Mu): return("Mu");
+        case(Afc): return("Afc");
+        case(St): return("St");
+        case(Jp): return("Jp");
+        case(Jz): return("Jz");
+        case(halt): return("halt");
+        case(nop): return("nop");
+        case(rrmovl): return("rrmovl");
+        case(irmovl): return("irmovl");
+        case(rmmovl): return("rmmovl");
+        case(mrmovl): return("mrmovl");
+        case(addl): return("addl");
+        case(subl): return("subl");
+        case(andl): return("andl");
+        case(xorl): return("xorl");
+        case(jmp): return("jmp");
+        case(je): return("je");
+        case(jg): return("jg");
+        case(call): return("call");
+        case(ret): return("ret");
+        case(pushl): return("pushl");
+        case(popl): return("popl");
+        case(0): return("");                   /* code 0: directive assembleur y86 */
+        default: return(NULL);
     };
 }
 
 
 /* affiche le quadruplet (pour generer code) avec separateur":" ;
 puis saute a la ligne */
-void ecrire_sep_quad(QUAD qd)
-{ if(strcmp(qd->ETIQ,"") == 0)       /* etiquette= mot vide */
-    {printf("%-10s: ","");}
-  else
-    {printf("%-10s:",qd->ETIQ);}
-  printf("%-6s:",nomop(qd->OP));
-  if (qd->ARG1!=NULL)
-    {printf("%-10s:",qd->ARG1);}
-  else
-    {printf("%-10s:","");}
-  if (qd->ARG2!=NULL)
-    {printf("%-10s:",qd->ARG2);}
-  else
-    {printf("%-10s:","");}
-  if (qd->RES!=NULL)
-    {printf("%-10s\n",qd->RES);}
-  else
-    {printf("\n");}
-  }
+void ecrire_sep_quad(QUAD qd) {
+    if(strcmp(qd->ETIQ, "") == 0) {     /* etiquette= mot vide */
+        printf("%-10s: ","");
+    }
+    else {
+        printf("%-10s:", qd->ETIQ);
+    }
+    printf("%-6s:",nomop(qd->OP));
+    if (qd->ARG1 != NULL) {
+        printf("%-10s:", qd->ARG1);
+    }
+    else {
+        printf("%-10s:", "");
+    }
+    if (qd->ARG2 != NULL) {
+        printf("%-10s:", qd->ARG2);
+    }
+    else {
+        printf("%-10s:", "");
+    }
+    if (qd->RES != NULL) {
+        printf("%-10s\n", qd->RES);
+    }
+    else {
+        printf("\n");
+    }
+}
 
 
 /* affiche la biliste de quad (avec separateurs) */
-void ecrire_sep_bilquad(BILQUAD bq)
-{QUAD qcour;
-  qcour=bq.debut;
-  while(qcour!=NULL)
-    {ecrire_sep_quad(qcour);
-      qcour=qcour->SUIV;}
+void ecrire_sep_bilquad(BILQUAD bq) {
+    QUAD qcour;
+    qcour = bq.debut;
+    while(qcour != NULL) {
+        ecrire_sep_quad(qcour);
+        qcour = qcour->SUIV;
+    }
 }
+
 /*-------------------------------------------------------------------*/
 /*------------------------pp-vers-quad------------------------------*/
 
